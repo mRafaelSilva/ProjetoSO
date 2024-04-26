24f2f90f77ba740f0fddaeefaa78a76a471ea20b
a104182 <a104182@alunos.uminho.pt>
2024-04-26 11:56:41 +0100

    ola

--- test_parallel.sh
+++ test_parallel.sh
@@ -10,6 +10,7 @@ $CLIENT execute 3000 -u "sleep 3" &
 $CLIENT execute 4000 -u "sleep 4" &
 $CLIENT execute 2000 -u "sleep 2" &
 $CLIENT execute 1000 -u "sleep 1" &
+$CLIENT execute 1000 -u "echo 'Hello World'" &
 
 # Aguarda todos os processos em background terminarem
 wait
