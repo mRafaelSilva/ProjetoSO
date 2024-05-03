68c775dc3195932e3d4db44d2633576faa720d57
Filipa Silva <a104167@alunos.uminho.pt>
2024-05-03 16:32:28 +0100

    tirei os printf's

--- orchestrator.c
+++ orchestrator.c
@@ -187,14 +187,24 @@ void execute_task(int task_index) {
     void handle_status_request() {
         for (int i = 0; i < task_count; i++) {
             if (tasks[i].status == 1) {
-                printf("Task %d a correr: %s\n", tasks[i].id, tasks[i].comando);
+                char msg[1024];
+                snprintf(msg, sizeof(msg), "Task %d a correr: %s\n", tasks[i].id, tasks[i].comando);
+                write(STDOUT_FILENO, msg, strlen(msg));
+            //    printf("Task %d a correr: %s\n", tasks[i].id, tasks[i].comando);
             } else if (tasks[i].status == 0) {
-                printf("Task %d à espera: %s\n", tasks[i].id, tasks[i].comando);
+                char msg[1024];
+                snprintf(msg, sizeof(msg), "Task %d à espera: %s\n", tasks[i].id, tasks[i].comando);
+                write(STDOUT_FILENO, msg, strlen(msg));
+            //    printf("Task %d à espera: %s\n", tasks[i].id, tasks[i].comando);
             } else if (tasks[i].status == 2) {
                 long seconds = tasks[i].hora_fim.tv_sec - tasks[i].hora_inicio.tv_sec;
                 long useconds = tasks[i].hora_fim.tv_usec - tasks[i].hora_inicio.tv_usec;
                 double duration = seconds + useconds / 1E6;
-                printf("Task %d completada: %s, Duração: %.6f segundos\n", tasks[i].id, tasks[i].comando, duration);
+                char msg[1024];
+                snprintf(msg, sizeof(msg), "Task %d completada: %s, Duração: %.6f segundos\n", tasks[i].id, tasks[i].comando, duration);
+                write(STDOUT_FILENO, msg, strlen(msg));
+                
+            //    printf("Task %d completada: %s, Duração: %.6f segundos\n", tasks[i].id, tasks[i].comando, duration);
             }
         }
     }
